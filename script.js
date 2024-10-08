dd410cff2dc7e1bf7e03274bbd8f54f6385bf379
FrankGrillo1991 <fsg12491@aol.com>
2024-10-08 06:26:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,47 @@ function togglePlayPause() {
     }
 }
 
+function updatePlayPauseButton(isPlaying) {
+    var playPauseButton = document.getElementById('play-pause');
+    playPauseButton.innerText = isPlaying ? 'Pause' : 'Play';
+}
+
+function playNextTrack() {
+    currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
+    changeTrack(tracks[currentTrackIndex], playlistItems[currentTrackIndex]);
+}
+
+function playPreviousTrack() {
+    currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
+    changeTrack(tracks[currentTrackIndex], playlistItems[currentTrackIndex]);
+}
+
+function changeTrack(source, element) {
+    audio.src = source;
+    audio.play();
+    updatePlayPauseButton(true);
+    updatePlaylistHighlight(element);
+}
+
+function updatePlaylistHighlight(element) {
+    if (element) {
+        playlistItems.forEach(li => li.classList.remove('playing'));
+        element.classList.add('playing');
+    }
+}
+
+audio.addEventListener('ended', playNextTrack);
+
+audio.addEventListener('timeupdate', function() {
+    var progressBar = document.getElementById('progress');
+    var percentage = Math.floor((100 / audio.duration) * audio.currentTime);
+    progressBar.style.width = percentage + '%';
+});
+
+// Adding click event listeners to playlist items
+playlistItems.forEach((item, index) => {
+    item.addEventListener('click', function() {
+        currentTrackIndex = index;
+        changeTrack(tracks[currentTrackIndex], this);
+    });
+});
